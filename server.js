91191d515aaeef355885f0127cb948b78898dd46
Steven Carpenter <stevegcarpenter@gmail.com>
2018-01-03 02:45:59 -0800

    Added a post endpoint for creating a new user.

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-'use strict'
+'use strict';
 
-const express = require('express')
+const express = require('express');
 const cors = require('cors');
 const pg = require('pg');
 const bodyParser = require('body-parser');
@@ -11,7 +11,6 @@ const PORT = process.env.PORT;
 const CLIENT_URL = process.env.CLIENT_URL;
 const client = new pg.Client(process.env.DATABASE_URL);
 const API_KEY = process.env.YOUTUBE_API_KEY;
-
 
 client.connect();
 client.on('error', err => console.error(err));
@@ -24,19 +23,36 @@ app.get('/', (req, res) => res.send('testing 1,2,3'));
 
 // Query database for user based on username
 app.get('/api/v1/users/:username', (req, res) => {
-  client.query(
-    `select * from users where username='${req.params.username}'`
-  )
-    .then(results => res.send(results.rows))
+  client
+    .query(`select * from users where username='${req.params.username}'`)
+    .then(data => {
+      console.log('username:', req.params.username);
+      console.log('data:', data.rows);
+      res.send(data.rows);
+    })
     .catch(console.error);
 });
 
-app.get('/api/v3/videos/search', (req, res) => {
-  console.log('in server get')
-  superAgent.get(`https://www.googleapis.com/youtube/v3/search?q=javascript&maxResults=1&part=snippet&key=${API_KEY}`)
-    .then( data => console.log(data))
-    .catch(console.error)
+// Create a new user in the database
+app.post('/api/v1/users', (req, res) => {
+  let { realname, username, password } = req.body;
+  client
+    .query(`insert into users(realname, username, password)
+      values($1, $2, $3)`,
+      [realname, username, password])
+    .then(results => res.sendStatus(201))
+    .catch(console.error);
 })
+
+app.get('/api/v3/videos/search', (req, res) => {
+  console.log('in server get');
+  superAgent
+    .get(
+      `https://www.googleapis.com/youtube/v3/search?q=javascript&maxResults=1&part=snippet&key=${API_KEY}`,
+    )
+    .then(data => console.log(data))
+    .catch(console.error);
+});
 
 app.get('*', (req, res) => res.redirect(CLIENT_URL));
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
